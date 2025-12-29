5151768aad48e99383e7bfdf28dfb90e09e8bbb6
DametraOwO <degamega35@gmail.com>
2025-12-30 02:12:14 +0700

    base structure
    (forgot to use github lol)
    Fetches data from the Aladhan API (Method 20 - Kemenag).
    Updates the time text for each prayer.
    Calculates the next prayer time and highlights it visually (larger size, primary color).

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/laporan')
+def laporan():
+    return render_template('laporan.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
